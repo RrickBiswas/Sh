f95e58f17793278763d673429c746620e5a6ca9a
Rrick_Biswas <106733947+RrickBiswas@users.noreply.github.com>
2024-12-23 13:23:00 +0530

    Update bms.sh

--- bms.sh
+++ bms.sh
@@ -7,24 +7,21 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
-NC='\033[0m' # No color (reset color)
+NC='\033[0m' # No color
 
-# Check if curl is installed, and install if not
+# Check dependencies
 if ! command -v curl &> /dev/null; then
     sudo apt update
     sudo apt install curl -y
 fi
-sleep 1
 
-# Check if bc is installed, and install if not
-echo -e "${BLUE}Checking your OS version...${NC}"
 if ! command -v bc &> /dev/null; then
     sudo apt update
     sudo apt install bc -y
 fi
-sleep 1
 
 # Check Ubuntu version
+echo -e "${BLUE}Checking your OS version...${NC}"
 UBUNTU_VERSION=$(lsb_release -rs)
 REQUIRED_VERSION=22.04
 
@@ -36,7 +33,7 @@ fi
 # Menu
 echo -e "${YELLOW}Select an action:${NC}"
 echo -e "${CYAN}1) Install Node${NC}"
-echo -e "${CYAN}2) Check Logs (exit logs with CTRL+C)${NC}"
+echo -e "${CYAN}2) Check Logs${NC}"
 echo -e "${CYAN}3) Update Node${NC}"
 echo -e "${CYAN}4) Restart Node${NC}"
 echo -e "${CYAN}5) Remove Node${NC}"
@@ -44,225 +41,104 @@ echo -e "${CYAN}5) Remove Node${NC}"
 echo -e "${YELLOW}Enter your choice:${NC} "
 read choice
 
+install_or_update() {
+    local action=$1
+    echo -e "${BLUE}${action} BlockMesh Node...${NC}"
+
+    # Stop existing service if updating
+    if [ "$action" = "Updating" ]; then
+        sudo systemctl stop blockmesh
+        sudo systemctl disable blockmesh
+        sudo rm /etc/systemd/system/blockmesh.service
+        sudo systemctl daemon-reload
+        rm -rf target
+    fi
+
+    # Install tar if needed
+    if ! command -v tar &> /dev/null; then
+        sudo apt install tar -y
+    fi
+    
+    # Download manager binary
+    wget https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.443/block-mesh-manager-x86_64-unknown-linux-gnu.tar.gz
+    
+    # Create directory structure
+    mkdir -p target/x86_64-unknown-linux-gnu/release/
+    
+    # Extract to target directory
+    tar -xzvf block-mesh-manager-x86_64-unknown-linux-gnu.tar.gz -C target/x86_64-unknown-linux-gnu/release/
+    rm block-mesh-manager-x86_64-unknown-linux-gnu.tar.gz
+
+    # Request credentials
+    echo -e "${YELLOW}Enter your BlockMesh email:${NC} "
+    read EMAIL
+    echo -e "${YELLOW}Enter your BlockMesh password:${NC} "
+    read -s PASSWORD
+
+    # Get user info
+    USERNAME=$(whoami)
+    HOME_DIR=$(eval echo ~$USERNAME)
+
+    # Create service file
+    sudo bash -c "cat <<EOT > /etc/systemd/system/blockmesh.service
+[Unit]
+Description=BlockMesh Manager Service
+After=network.target
+
+[Service]
+User=$USERNAME
+Environment=\"BLOCKMESH_EMAIL=$EMAIL\"
+Environment=\"BLOCKMESH_PASSWORD=$PASSWORD\"
+ExecStart=$HOME_DIR/target/x86_64-unknown-linux-gnu/release/block-mesh-manager
+WorkingDirectory=$HOME_DIR/target/x86_64-unknown-linux-gnu/release
+Restart=on-failure
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOT"
+
+    # Start service
+    sudo systemctl daemon-reload
+    sudo systemctl enable blockmesh
+    sudo systemctl start blockmesh
+
+    echo -e "${GREEN}${action} complete and node started!${NC}"
+    
+    echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+    echo -e "${YELLOW}Command to check logs:${NC}" 
+    echo "sudo journalctl -u blockmesh -f"
+    echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
+    
+    # Show logs
+    sudo journalctl -u blockmesh -f
+}
+
 case $choice in
     1)
-        echo -e "${BLUE}Installing BlockMesh Node...${NC}"
-
-        # Check if tar is installed, and install if not
-        if ! command -v tar &> /dev/null; then
-            sudo apt install tar -y
-        fi
-        sleep 1
-        
-        # Download BlockMesh binary
-        wget https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.358/blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
-
-        # Extract the archive
-        tar -xzvf blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
-        sleep 1
-
-        # Remove the archive
-        rm blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
-
-        # Navigate to the folder
-        cd target/x86_64-unknown-linux-gnu/release/
-
-        # Request user input
-        echo -e "${YELLOW}Enter your BlockMesh email:${NC} "
-        read EMAIL
-        echo -e "${YELLOW}Enter your BlockMesh password:${NC} "
-        read PASSWORD
-
-        # Get the current username and home directory
-        USERNAME=$(whoami)
-        HOME_DIR=$(eval echo ~$USERNAME)
-
-        # Create or update the service file
-        sudo bash -c "cat <<EOT > /etc/systemd/system/blockmesh.service
-[Unit]
-Description=BlockMesh CLI Service
-After=network.target
-
-[Service]
-User=$USERNAME
-ExecStart=$HOME_DIR/target/x86_64-unknown-linux-gnu/release/blockmesh-cli login --email $EMAIL --password $PASSWORD
-WorkingDirectory=$HOME_DIR/target/x86_64-unknown-linux-gnu/release
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
-EOT"
-
-        # Reload systemd services and enable BlockMesh
-        sudo systemctl daemon-reload
-        sleep 1
-        sudo systemctl enable blockmesh
-        sudo systemctl start blockmesh
-
-        # Final output
-        echo -e "${GREEN}Installation complete and node started!${NC}"
-
-        # Final message
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        echo -e "${YELLOW}Command to check logs:${NC}" 
-        echo "sudo journalctl -u blockmesh -f"
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        sleep 2
-
-        # Check logs
-        sudo journalctl -u blockmesh -f
+        install_or_update "Installing"
         ;;
-
     2)
-        # Check logs
         sudo journalctl -u blockmesh -f
         ;;
-
     3)
-        echo -e "${BLUE}Updating BlockMesh Node...${NC}"
-
-        # Stop the service
-        sudo systemctl stop blockmesh
-        sudo systemctl disable blockmesh
-        sudo rm /etc/systemd/system/blockmesh.service
-        sudo systemctl daemon-reload
-        sleep 1
-
-        # Remove old node files
-        rm -rf target
-        sleep 1
-
-        # Download the new BlockMesh binary
-        wget https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.339/blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
-
-        # Extract the archive
-        tar -xzvf blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
-        sleep 1
-
-        # Remove the archive
-        rm blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
-
-        # Navigate to the folder
-        cd target/x86_64-unknown-linux-gnu/release/
-
-        # Request user input for update
-        echo -e "${YELLOW}Enter your BlockMesh email:${NC} "
-        read EMAIL
-        echo -e "${YELLOW}Enter your BlockMesh password:${NC} "
-        read PASSWORD
-
-        # Get the current username and home directory
-        USERNAME=$(whoami)
-        HOME_DIR=$(eval echo ~$USERNAME)
-
-        # Create or update the service file
-        sudo bash -c "cat <<EOT > /etc/systemd/system/blockmesh.service
-[Unit]
-Description=BlockMesh CLI Service
-After=network.target
-
-[Service]
-User=$USERNAME
-ExecStart=$HOME_DIR/target/x86_64-unknown-linux-gnu/release/blockmesh-cli login --email $EMAIL --password $PASSWORD
-WorkingDirectory=$HOME_DIR/target/x86_64-unknown-linux-gnu/release
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
-EOT"
-
-        # Restart the service
-        sudo systemctl daemon-reload
-        sleep 1
-        sudo systemctl enable blockmesh
-        sudo systemctl restart blockmesh
-
-        # Final output
-        echo -e "${GREEN}Update complete and node restarted!${NC}"
-
-        # Final message
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        echo -e "${YELLOW}Command to check logs:${NC}" 
-        echo "sudo journalctl -u blockmesh -f"
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        sleep 2
-
-        # Check logs
-        sudo journalctl -u blockmesh -f
+        install_or_update "Updating"
         ;;
-
     4)
         echo -e "${BLUE}Restarting BlockMesh Node...${NC}"
-
-        # Stop the service
-        sudo systemctl stop blockmesh
-
-        # Navigate to the folder
-        cd target/x86_64-unknown-linux-gnu/release/
-
-        # Request user input
-        echo -e "${YELLOW}Enter your BlockMesh email:${NC} "
-        read EMAIL
-        echo -e "${YELLOW}Enter your BlockMesh password:${NC} "
-        read PASSWORD
-
-        # Get the current username and home directory
-        USERNAME=$(whoami)
-        HOME_DIR=$(eval echo ~$USERNAME)
-
-        # Update the service file
-        sudo bash -c "cat <<EOT > /etc/systemd/system/blockmesh.service
-[Unit]
-Description=BlockMesh CLI Service
-After=network.target
-
-[Service]
-User=$USERNAME
-ExecStart=$HOME_DIR/target/x86_64-unknown-linux-gnu/release/blockmesh-cli login --email $EMAIL --password $PASSWORD
-WorkingDirectory=$HOME_DIR/target/x86_64-unknown-linux-gnu/release
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
-EOT"
-
-        # Restart the service with new credentials
-        sudo systemctl daemon-reload
-        sleep 1
         sudo systemctl restart blockmesh
-
-        # Final output
-        echo -e "${GREEN}Restart complete and node started with new credentials!${NC}"
-
-        # Final message
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        echo -e "${YELLOW}Command to check logs:${NC}" 
-        echo "sudo journalctl -u blockmesh -f"
-        echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-        sleep 2
-
-        # Check logs
+        echo -e "${GREEN}Node restarted!${NC}"
         sudo journalctl -u blockmesh -f
         ;;
-
     5)
         echo -e "${BLUE}Removing BlockMesh Node...${NC}"
-
-        # Stop and disable the service
         sudo systemctl stop blockmesh
         sudo systemctl disable blockmesh
-
-        # Remove the service file
         sudo rm /etc/systemd/system/blockmesh.service
         sudo systemctl daemon-reload
-
-        # Remove BlockMesh files
         rm -rf target
-        sleep 1
-
         echo -e "${GREEN}Node successfully removed!${NC}"
         ;;
-
     *)
         echo -e "${RED}Invalid choice, exiting...${NC}"
         ;;
